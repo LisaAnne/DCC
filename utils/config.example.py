5ac50cfcead46e8b39227c8c995d02727a0d6ab6
LisaAnne <lisa.a.hendricks@gmail.com>
2016-06-08 23:25:58 -0700

    utils

---
+++ utils/config.example.py
@@ -0,0 +1,11 @@
+caffe_dir = 'caffe/'  #path to your caffe directory
+pycaffe_dir = caffe_dir + 'python/'  #path to your pycaffe directory
+
+feature_dir = 'lexical_features/' #path to store extracted features
+coco_annotations = 'annotations/'  #download these annotations from the MSCOCO site
+coco_images_root = 'coco_images/' #download these images from MSCOCO site
+imagenet_images_root = 'dcc_imagenet/' #subset of imagenet dataset collected for DCC
+coco_caption_eval = 'coco-caption-eval/'  #eval tools from MSCOCO site
+caption_weights_root = 'snapshots/' #wherever you store your snapshots
+pretrained_lm = 'trained_models/language_models/'
+
