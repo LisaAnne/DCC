da1dc236138380edcf3ea6db8053fa825a289523
LisaAnne <lisa.a.hendricks@gmail.com>
2017-04-17 23:22:27 -0700

    changed baseline bash file

--- run_dcc_coco_baseline_vgg.sh
+++ run_dcc_coco_baseline_vgg.sh
@@ -2,4 +2,4 @@
 
 export PYTHONPATH='utils/:$PYTHONPATH'
 
-caffe/build/tools/caffe train -solver prototxts/dcc_coco_baseline_vgg.solver.prototxt -weights trained_models/language_models/mrnn.direct_iter_110000.caffemodel -gpu 0
+caffe/python/train.py --solver prototxts/dcc_coco_baseline_vgg.solver.prototxt --weights trained_models/language_models/mrnn.direct_iter_110000.caffemodel --gpu 0
